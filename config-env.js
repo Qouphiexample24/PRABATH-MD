105d54f8099f7fa2e7a23867c9289a7f511d105b
Qouphiexample24 <166181217+Qouphiexample24@users.noreply.github.com>
2024-04-14 02:26:40 +0000

    Update config-env.js

--- config-env.js
+++ config-env.js
@@ -20,9 +20,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Enter your session-id",
-BOT_NUMBER: process.env.BOT_NUMBER || "Enter your botnumber (without + mark and spaces)",
-PASSWORD: process.env.PASSWORD || "Enter you like password",
-GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Enter your github username",
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~tu0zBRCa#KvRnPcEpUMCraOJsAMIlzBP_oOcovtaAPDqMGH1ObrQ",
+BOT_NUMBER: process.env.BOT_NUMBER || "233503744057",
+PASSWORD: process.env.PASSWORD || "gideon18$",
+GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Qouphiexample24",
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "Enter your GITHUB_AUTH_TOKEN"
 };
